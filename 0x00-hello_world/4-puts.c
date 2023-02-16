9f5f7f10f9dfa818c42f9aec25e3df039c23f068
Martin-do <martinjaiyeola40@gmail.com>
2023-02-16 04:17:34 -0800

    "hello puts betty linted"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*This is the main function*/
+/**
+ * main - This is the main function
+ * @void: just a void param
+ * Return: The value returns 0 if no error
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
