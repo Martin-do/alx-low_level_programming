b1b9ee111096beb505ac8ac4d25efdc33b9db58d
Martin-do <martinjaiyeola40@gmail.com>
2023-02-16 04:25:41 -0800

    "hello prinf betty linted"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is my main function
+ * Return: return 0 if no erro
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
