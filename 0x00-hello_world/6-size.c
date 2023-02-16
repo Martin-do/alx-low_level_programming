361748bc284b22fdddb4b0500d1aab836f91c951
Martin-do <martinjaiyeola40@gmail.com>
2023-02-16 05:05:06 -0800

    "about size linted"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * main - this is the main function
  * Return: this reurns 0 if no error
  */
-int main()
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
