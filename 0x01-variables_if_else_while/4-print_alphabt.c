684b1e3f7e5614f9bf787be99110d6652eb2428b
Martin-do <martinjaiyeola40@gmail.com>
2023-02-17 12:09:58 -0800

    "task 4 and 5"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ * Return: a return value of 0 if no error
+ */
+int main(void)
+{
+	char alphaL = 'a';
+
+	for (; alphaL <= 'z'; alphaL++)
+	{
+		if ((alphaU == 'q') || (alphaU == 'e'))
+		{
+			alphaL++;
+		}
+		putchar(alphaL);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ * Return: a return value of 0 if no error
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
