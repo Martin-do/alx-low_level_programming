a15c18dd0e06eb6e25913e006a01f50194a8e370
Martin-do <martinjaiyeola40@gmail.com>
2023-02-17 04:56:12 -0800

    "alpABET"

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ * Return: a return value of 0 if no error
+ */
+int main(void)
+{
+	char alphaL = 'a';
+	char alphaU = 'A';
+
+	for (; alphaU <= 'Z'; alphaU++)
+	{
+		for (; alphaL <= 'z'; alphaL++)
+		{
+			putchar(alphaU);
+		}
+		putchar(alphaL);
+	}
+	putchar('\n');
+	return (0);
+}
