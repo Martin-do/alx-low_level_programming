a7ec2f24fe147c789e9a26b284cdfed58eb56c4b
Martin-do <martinjaiyeola40@gmail.com>
2023-02-17 04:29:12 -0800

    "putchar"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ * Return: a return value of 0 if no error
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
