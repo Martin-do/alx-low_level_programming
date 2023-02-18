34bcfb60f646413ad739fa15f19cb0e740b898be
Martin-do <martinjaiyeola40@gmail.com>
2023-02-18 12:44:19 -0800

    "combo combo"

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,30 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b, c, d, num, sum;
 
+	sum = a + b + c + d;
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			num = b + 1;
+			for (c = a; c <= 9; c++)
 			{
-				for (d = 1; d <= 9; d++)
+				for (d = num; d <= 9; d++)
 				{
 					putchar('0' + a);
 					putchar('0' + b);
 					putchar(' ');
 					putchar('0' + c);
 					putchar('0' + d);
-					if (c + d < 18)
+					if (((a != 9) & (sum < 36)) || (a == 9) & (sum < 35))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-			}
-			if (a + b < 17)
-			{
-				putchar(',');
+				num = 0;
 			}
 		}
 	}
