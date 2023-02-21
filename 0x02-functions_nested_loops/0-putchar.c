d71a4fd210bd14f96aa31a56a46701889d0c1839
Martin-do <martinjaiyeola40@gmail.com>
2023-02-20 23:36:36 -0800

    "first task"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: retunr 0 if no error
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	return (0);
+}
