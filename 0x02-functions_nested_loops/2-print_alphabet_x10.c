be83a2e6d550b96fe1ff30690089ff070ee4ac65
Martin-do <martinjaiyeola40@gmail.com>
2023-02-21 06:39:41 -0800

    "alphabets 10 times"

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabets 10 times
+ * Return: return void
+ */
+void print_alphabet_x10(void)
+{
+	int num = 0;
+
+	for (; num <= 10; num++)
+	{
+		_putchar(print_alphabet(void));
+	}
+	_putchar('\n');
+}
