65c6712d51ca6658013661ff8004a600bedf4a2e
Martin-do <martinjaiyeola40@gmail.com>
2023-02-21 06:01:30 -0800

    "i dunno"

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * print_alphabet - this prints out lowercase alphabets
+ * Return: return 0 if no error
+ */
+
+void print_alphabet(void)
+{
+	int alpha = 'a';
+
+	for (alpha; alpha <= 'z'; alpha++)
+	{
+	_putchar(alpha);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "_putchar.c"
+#include "1-alphabet.c"
