eebf272d41465c480d2ebebef39ce2e7665ffc1b
Martin-do <martinjaiyeola40@gmail.com>
2023-02-21 18:56:33 -0800

    "sum multiple"

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - sum of multiples
+ * Return: return a value
+ */
+int main(void)
+{
+	int n, mod3, mod5, sum;
+
+	n = 0;
+	mod3 = n % 3;
+	mod5 = n % 5;
+	sum = 0;
+	for (; n < 1024; n++)
+	{
+		if ((mod3 == 0) && (mod5 == 0))
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
