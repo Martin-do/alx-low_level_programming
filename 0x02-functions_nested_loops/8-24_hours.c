ac18e712e3e64816fea901f50d8e1f254e2fd251
Martin-do <martinjaiyeola40@gmail.com>
2023-02-21 16:05:32 -0800

    "task 8"

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,11 +6,19 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b, c, d, num;
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		if (a != 2)
+		{
+		num = 9;
+		}
+		else
+		{
+		num = 3;
+		}
+		for (b = 0; b <= num; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
