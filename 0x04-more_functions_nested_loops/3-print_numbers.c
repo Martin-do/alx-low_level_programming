85e88ac89a5009e6f832e7aa8bf3bd2fabdfe50c
Martin-do <martinjaiyeola40@gmail.com>
2023-02-23 10:55:05 -0800

    "task 3"

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+	{
+		_putchar(num + 48)
+	}
+	_putchar('\n')
+}
