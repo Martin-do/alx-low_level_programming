6ae25cab265230ed8731f4a7f38d9899628e8d71
Martin-do <martinjaiyeola40@gmail.com>
2023-02-24 07:21:56 -0800

    "task 10"

---
+++ 0x04-more_functions_nested_loops/10-print-triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - to print a triangle
+ * @size: the aize of the triangle
+ */
+void print_triangle(int size)
+{
+	int num, hash, times;
+
+	if (size != 0 && size > 0)
+	{
+		for (num = 0; num < size; num++)
+		{
+			for (hash = 0; hash + 1 < size; hash++)
+			{
+				_putchar(' ');
+			}
+			for (times = 0; times <= num; times++)
+			{
+				_putchar('#');
+			}
+			hash = 0;
+			times = 0;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
