98862abfa82dd5ac5ee14c40b6411e432ab4b8ca
Martin-do <martinjaiyeola40@gmail.com>
2023-02-24 06:06:46 -0800

    "task 9"

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - fizz buzz program
+ * Return: returns a value
+ */
+int main(void)
+{
+	int num = 1;
+
+	for (; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ");
+		}
+	}
+	return (0);
+}
