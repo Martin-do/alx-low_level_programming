50d83a58c63ad87da1fab4b63a45f906cec1a9df
Martin-do <martinjaiyeola40@gmail.com>
2023-02-27 13:42:25 -0800

    "kkk"

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,38 +14,38 @@ void keygen(int N)
 	int randomizer = 0;
 
 	srand((unsigned int)(time(NULL)));
-	char numbers[] = "0123456789";
+	char num[] = "0123456789";
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 	char symbols[] = "!@#$^&*?";
-	char password[N];
+	char passwd[N];
 
 	randomizer = rand() % 4;
 	for (i = 0; i < N; i++)
 	{
 		if (randomizer == 1)
 		{
-			password[i] = numbers[rand() % 10];
+			passwd[i] = num[rand() % 10];
 			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			printf("%c", passwd[i]);
 		}
 		else if (randomizer == 2)
 		{
-			password[i] = symbols[rand() % 8];
+			passwd[i] = symbols[rand() % 8];
 			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			printf("%c", passwd[i]);
 		}
 		else if (randomizer == 3)
 		{
-			password[i] = LETTER[rand() % 26];
+			passwd[i] = LETTER[rand() % 26];
 			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			printf("%c", passwd[i]);
 		}
 		else
 		{
-			password[i] = letter[rand() % 26];
+			passwd[i] = letter[rand() % 26];
 			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			printf("%c", passwd[i]);
 		}
 	}
 }
