66bfc71a994a4c168d52b577bca77e4900874e50
Martin-do <martinjaiyeola40@gmail.com>
2023-02-27 08:34:53 -0800

    "tasj 2"

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return length of a string
+ * @s: a string
+ * Return: returns a value
+ */
+int _strlen(char *s)
+{
+	int num, count;
+
+	for (num = 0; s[num] != s[-1]; num++)
+	{
+		count++
+	}
+
+	return (count);
+}
