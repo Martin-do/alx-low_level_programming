37d3d8bc14cf189e8926b42989c84381ba6e6c57
Martin-do <martinjaiyeola40@gmail.com>
2023-02-27 11:07:26 -0800

    "task 7"

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half numbers
+ * @str: charactes
+ */
+void puts_half(char *str)
+{
+	int num, count, half;
+
+	num = 0;
+	count = 0;
+	for (; str[num] != '\0'; num++)
+	{
+		count++;
+	}
+
+	half = count / 2;
+
+	if (half % 2 != 0)
+	{
+		for (; str[half] != '\0'; half++)
+		{
+			_putchar(str[half]);
+		}
+	}
+	else
+	{
+		half = (count - 1) / 2;
+		for (; str[half] != '\0'; half++)
+		{
+			_putchar(str[half]);
+		}
+	}
+	_putchar('\n');
+}
