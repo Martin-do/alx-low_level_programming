e83f8dc0b960abb05073b64595a46edc84c0c6f2
Martin-do <martinjaiyeola40@gmail.com>
2023-02-27 13:53:57 -0800

    "task 5"

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,4 +5,27 @@
  * @s: a string
  */
 void rev_string(char *s)
+{
+	int num, count, a, b;
 
+	num = 0;
+	count = 0;
+
+	for (; s[num] != s[-1]; num++)
+	{
+		if (s[num] > 0)
+		{
+			count++;
+		}
+	}
+	count -= 1;
+	half = count / 2;
+	for (; count >= 0; count--)
+	{
+		a = s[count - half];
+		b = s[half];
+		s[half] = a;
+		s[count - half] = b;
+		half--;
+	}
+}
