baf98842c6fbee08d22dd425c98da4670792829f
Martin-do <martinjaiyeola40@gmail.com>
2023-02-27 13:36:19 -0800

    "keygen"

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include "main.h"
+#include <time.h>
+#include <math.h>
+
+/**
+ * keygen - generate password
+ * @N: length of password
+ * Return: return value
+ */
+void keygen(int N)
+{
+	int i = 0;
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[N];
+
+	randomizer = rand() % 4;
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+int main(void)
+{
+	int N = 10;
+
+	randomPasswordGeneration(N);
+	return (0);
+}
+
