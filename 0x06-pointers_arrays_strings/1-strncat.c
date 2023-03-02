c244e88d96eaac7c5b51b5300ec3b818a0ceca21
Martin-do <martinjaiyeola40@gmail.com>
2023-03-02 12:18:31 -0800

    "tasl 1"

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates strig with size consideration
+ * @dest: output
+ * @src: input string
+ * @n: size in byte
+ * Return: returns the output as a pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int num = 0;
+	int count = 0;
+	int size = 0;
+
+	for (; dest[num] != '\0'; num++)
+	{
+		count++;
+	}
+	num = 0;
+	for (; size <= n; num++)
+	{
+		size += sizeof(src + num);
+		dest[count + num] = src[num];
+	}
+	dest[count + num] = '\0';
+	return (dest);
+}
