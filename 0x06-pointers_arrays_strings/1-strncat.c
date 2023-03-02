b98ba003f3ba084a82bf68ab84cdc393f6e8ada7
Martin-do <martinjaiyeola40@gmail.com>
2023-03-02 13:10:52 -0800

    "aa"

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int num = 0;
 	int count = 0;
-	int size = 0;
 
 	for (; dest[num] != '\0'; num++)
 	{
 		count++;
 	}
 	num = 0;
-	for (; size <= n && src[num] != '\0'; num++)
+	for (; num < n && src[num] != '\0'; num++)
 	{
 		dest[count + num] = src[num];
-		size += sizeof(src + num);
 	}
 	dest[count + num] = '\0';
 	return (dest);
