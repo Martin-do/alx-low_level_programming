679334c9eaba693e79930df02202170575aa753c
Martin-do <martinjaiyeola40@gmail.com>
2023-03-02 18:05:44 -0800

    "4"

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int half = n / 2;
+	int num, first, last;
+
+	for (num = 0; num < half; num++)
+	{
+		first = a[n - num];
+		last = a[num];
+		a[num] = first;
+		a[n - num] = last;
+	}
+}
