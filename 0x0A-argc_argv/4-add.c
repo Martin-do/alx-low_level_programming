422622745f6e037c13ba7f8057e779e556b2c59b
Martin-do <martinjaiyeola40@gmail.com>
2023-03-10 20:47:43 -0800

    "task 4"

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - adds numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return value
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int count = 0;
+
+	if (argc - 1 == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc > 1)
+	{
+		while (count != argc)
+		{
+			sum += atoi(argv[count]);
+			count++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
