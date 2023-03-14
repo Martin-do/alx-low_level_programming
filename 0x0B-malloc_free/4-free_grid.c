558e00c070ac61dde91ec1b74b943458a741d9cd
Martin-do <martinjaiyeola40@gmail.com>
2023-03-14 11:22:34 -0700

    "task 4"

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * free_grid - frees a grid previously created
+ * @grid: pointer to a grid
+ * @height: number of rows in grid
+ * Return: returns something
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+
+}
