f793e50d8e73d9e9ea0ab9e5ca8c0de2d33aa056
Martin-do <martinjaiyeola40@gmail.com>
2023-03-20 10:43:02 -0700

    "changed the macro definition"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,4 +24,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* MAIN_H */
+#endif /* DOG_H */
