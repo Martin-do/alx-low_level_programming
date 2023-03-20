087f2056d9076c378c736c1aa7cc1479975eee8a
Martin-do <martinjaiyeola40@gmail.com>
2023-03-20 10:01:48 -0700

    "added description for the new type"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a struct type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	loat age;
 	char *owner;
 };
+
+#endif /* MAIN_H */
