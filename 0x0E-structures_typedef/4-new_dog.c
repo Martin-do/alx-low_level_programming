5af44d0584c8b84283a867d74b850799b0fd7ce0
Martin-do <martinjaiyeola40@gmail.com>
2023-03-21 19:20:25 -0700

    "added curly bracket"

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,8 +49,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	bingo = malloc(sizeof(dog_t));
 	if (bingo == NULL)
+	{
 		free(bingo);
 		return (NULL);
+	}
 
 	dog_name = _strdup(name);
 	if (dog_name == NULL)
