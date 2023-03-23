7f91aa9a94abcaad14eb304bce79fd2b1db32b87
Martin-do <martinjaiyeola40@gmail.com>
2023-03-22 19:04:19 -0700

    "added these file: 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h; to calculate operation of two integers"

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: returns 0
+*/
+int op_add(int a, int b)
+{
+	printf("%d\n", a + b);
+	return (0);
+}
+
+/**
+ * op_sub - adds two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: returns 0
+*/
+int op_sub(int a, int b)
+{
+	printf("%d\n", a - b);
+	return (0);
+}
+
+/**
+ * op_mul - adds two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: returns 0
+*/
+int op_mul(int a, int b)
+{
+	printf("%d\n", a * b);
+	return (0);
+}
+
+/**
+ * op_div - adds two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: returns 0
+*/
+int op_div(int a, int b)
+{
+	printf("%d\n", a / b);
+	return (0);
+}
+
+/**
+ * op_mod - adds two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: returns 0
+*/
+int op_mod(int a, int b)
+{
+	printf("%d\n", a % b);
+	return (0);
+}
