731655190742732514208a6d6152061cd130fc12
Martin-do <martinjaiyeola40@gmail.com>
2023-03-22 19:09:51 -0700

    "added 3-main.c file which was omitted before"

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+
+/**
+ * main - main function that performs operation
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: returns result of operation
+*/
+int main(int argc, char **argv)
+{
+	int num1, num2;
+
+	char *modulus = "%";
+	char *division = "/";
+
+	if (argc - 1 != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		print("Error\n");
+		exit(99);
+	}
+	if (((argv[2] == d) || (argv[2] == m)) && (argv[3] == 0))
+	{
+		printf("Error3\n");
+		exit(100);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	return (get_op_func(argv[2])(num1, num2));
+}
