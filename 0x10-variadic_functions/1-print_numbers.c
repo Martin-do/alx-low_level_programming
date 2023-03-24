4c692455c6398c4667e939ef225473f85f6b0cf0
Martin-do <martinjaiyeola40@gmail.com>
2023-03-24 04:59:13 -0700

    "removed unneccesary space"

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if ((separator != NULL) && (i != n - 1))
-			printf("%d%s ", va_arg(ap, int), separator);
+			printf("%d%s", va_arg(ap, int), separator);
 		else
 			printf("%d", va_arg(ap, int));
 	}
