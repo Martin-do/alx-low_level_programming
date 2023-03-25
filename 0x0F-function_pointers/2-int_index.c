f756a5418a61a6bf4ebe377302b6836a4774ee70
Martin-do <martinjaiyeola40@gmail.com>
2023-03-24 18:51:52 -0700

    consideration for NULL input

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,6 +16,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
@@ -23,5 +26,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		}
 	}
-	return (-1);
+	return (-);
 }
