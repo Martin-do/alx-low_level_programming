2c556856a6210f9e3d7130ac68f7d1a1aac65a06
Martin-do <martinjaiyeola40@gmail.com>
2023-04-04 15:53:07 -0700

    "function that deletes a node"

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes a node and returns the data of the node
+ * @head: a listint_t list
+ * Return: returns the data of the deleted node
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *inter;
+	int i = 0;
+
+	inter = *head;
+	if (head != NULL)
+	{
+		*head = (*head)->next;
+		i = inter->n;
+		free(inter);
+		return (i);
+	}
+	else
+		return (0);
+}
