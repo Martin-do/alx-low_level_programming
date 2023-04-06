61158df65f568d5b4568ca395f23a0ea30eb15ee
Martin-do <martinjaiyeola40@gmail.com>
2023-04-05 20:09:48 -0700

    "added consideration for NULL"

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,11 +2,14 @@
 /**
  * free_listint2 - frees a listint_t list
  * @head: a listint_t list
+ * Return: return nothing
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *inter, *new;
 
+	if (head == NULL)
+		return;
 	inter = *head;
 	while (inter->next != NULL)
 	{
