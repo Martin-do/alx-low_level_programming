1ae8b6ca21f04d6b8e6a9d584413f7a5c6665583
Martin-do <martinjaiyeola40@gmail.com>
2023-04-15 05:36:04 -0700

    "refactored code - put error message before exit codes"

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ int file_check_b(char *b)
 {
 	int file_b;
 
-	file_b = open(b, O_CREAT | O_WRONLY | O_TRUNC, 664);
+	file_b = open(b, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_b == -1)
 	{
 		exit(99);
@@ -51,8 +51,8 @@ int main(int argc, char **argv)
 
 	if (argc - 1 != 2)
 	{
-		exit(97);
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	file_frm = file_check_a(argv[1]);
 	file_to = file_check_b(argv[2]);
@@ -61,24 +61,24 @@ int main(int argc, char **argv)
 		file_cp = write(file_to, buffer, content);
 		if (file_cp == -1)
 		{
-			exit(99);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (content == -1)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (close(file_frm) == -1)
 	{
-		exit(100);
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_frm);
+		exit(100);
 	}
 	if (close(file_to) == -1)
 	{
-		exit(100);
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
 	}
 	return (0);
 }
