40a38ccd6af99804a65ec7ce4ff28e7c041b1868
Martin-do <martinjaiyeola40@gmail.com>
2023-04-22 07:26:54 -0700

    "corrected betty style"

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,11 +3,11 @@
  * get_endianness - helps to get the endianness of a machine
  * Return: returns 1 if Little Endian or 0 if Big Endian
 */
-int get_endianness()
+int get_endianness(void)
 {
 	int x = 1;
 
-	char *y = (char*)&x;
+	char *y = (char *)&x;
 
 	return (*y);
 }
