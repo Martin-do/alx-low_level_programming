a53cb98a2b95c97567b77ba1714eced07cce4a14
Martin-do <martinajaiyeola40@gmail.com>
2023-08-17 09:52:57 -0700

    "handled exception if list is empty and fixed loop issue of skipping adding the value of the first node"

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,18 +4,21 @@
  * sum_dlistint - a function that returns the sum of
  * all the data (n) of a dlistint_t linked list.
  * @head: pointer to the beginning of the dlistint_t list
- * Return: returns the sum of all the data (n) of a
- * dlistint_t linked list or 0 if empty
+ * Return: returns the sum of all the data (n) of a dlistint_t linked list or 0 if empty
 */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
 	int sum = 0;
 
-	while (temp->next != NULL)
+	if (head == NULL)
+		return (0);
+
+
+	while (temp != NULL)
 	{
-		temp = temp->next;
 		sum += temp->n;
+		temp = temp->next;
 	}
 	return (sum);
 }
