ca9af719f8082a9baa58c3f347f8503d8fe8925e
Martin-do <martinajaiyeola40@gmail.com>
2023-08-17 09:18:47 -0700

    "refactored code to handle more exceptions"

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,12 +12,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL, *temp = *head;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->prev = NULL;
 	new->n = n;
 	new->next = temp;
+	if (temp != NULL)
+		temp->prev = new;
 	*head = new;
 	return (new);
 }
